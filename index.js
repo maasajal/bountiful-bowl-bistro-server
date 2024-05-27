bc8107959ab7d29752b4eada4917a3d470df57b3
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-05-27 23:29:20 +0300

    BBB server create with MongoDB

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Bountiful Bowl Bistro!");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jd9hrzt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+};
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Bountiful Bowl Bistro server running on port ${port}!`);
+});
