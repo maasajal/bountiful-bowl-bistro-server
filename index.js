afe895c1167e4afcdba16b71cbc301d23f13a68c
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-05-29 00:46:01 +0300

    delete cart item

--- index.js
+++ index.js
@@ -54,6 +54,13 @@ const run = async () => {
       res.send(result);
     });
 
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
