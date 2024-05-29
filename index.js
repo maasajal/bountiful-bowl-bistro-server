674a3a60c21181713c07409ab4cf4e6871778650
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-05-29 16:57:18 +0300

    Google login user info added

--- index.js
+++ index.js
@@ -52,6 +52,11 @@ const run = async () => {
 
     app.post("/users", async (req, res) => {
       const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already exists", insertedId: null });
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
